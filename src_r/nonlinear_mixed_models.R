655bfb028bd0e96e58d2568af0dea4d551255214
YalmazAbdullah <yaserver@NBP-WorkServer.>
2024-10-25 16:57:34 -0600

    starting analysis

--- src_r/nonlinear_mixed_models.R
+++ src_r/nonlinear_mixed_models.R
@@ -6,24 +6,87 @@ library(lme4)
 library(robustlmm)
 library(ggplot2)
 library(emmeans)
+library(nlme)
+library(lmerTest)
+library(parameters)
 
+################
+# Prepare Data #
+################
 # Read Data
-data <- read.csv("./output/dakshina.csv")
-data <- read.csv("./output/roUrParl.csv")
+data <- read.csv("./output/data/dakshina.csv")
+data <- read.csv("./output/data/roUrParl.csv")
 view(data)
 
 # Add Hand-Finger combined column
 # Each key uses only one Hand-Finger combination
-data$HandFinger <- paste(data$Hand, data$Finger)
+data$Hand_Finger <- paste(data$Hand, data$Finger)
 view(data)
 
-#Add 1 smoothing
+# Add 1 smoothing
 crulp <- subset(data, Keyboard == "CRULP")
 roman <- subset(data, Keyboard == "Roman")
 windows <- subset(data, Keyboard == "Windows")
 
-crulp$Freq_Add1 <- (crulp$Frequency+1)/(sum(crulp$Frequency)+length(crulp$Frequency))
-roman$Freq_Add1 <- (roman$Frequency+1)/(sum(roman$Frequency)+length(roman$Frequency))
-windows$Freq_Add1 <- (windows$Frequency+1)/(sum(windows$Frequency)+length(windows$Frequency))
+crulp$Prob_Add1 <- (crulp$Frequency+1)/(sum(crulp$Frequency)+length(crulp$Frequency))
+roman$Prob_Add1 <- (roman$Frequency+1)/(sum(roman$Frequency)+length(roman$Frequency))
+windows$Prob_Add1 <- (windows$Frequency+1)/(sum(windows$Frequency)+length(windows$Frequency))
 
 data <- rbind(crulp, roman, windows)
+view(data)
+
+# Total distance calculation
+data$Base <- data$Distance*data$Prob_Add1
+view(data)
+
+# Summary stats
+data %>%
+  group_by(Keyboard) %>%
+  get_summary_stats(Base, type = "common") 
+
+
+#################
+# Friedman test #
+#################
+res.fried <- data %>% friedman_test(Base ~ Keyboard | Key)
+res.fried
+
+# Effect size
+data %>% friedman_effsize(Base ~ Keyboard |Key) 
+
+# Post-hoc
+pwc <- data %>%
+  wilcox_test(Base ~ Keyboard, paired = TRUE, p.adjust.method = "hommel")
+pwc 
+
+ggboxplot(data, x = "Keyboard", y = "Base", add = "jitter")
+
+
+##################################
+# Non linear mixed effects Model #
+##################################
+# build model
+mod <- lme(Base ~  Keyboard *Hand_Finger , random = ~ Keyboard | Key, data =data)
+mod
+
+# format output
+tut <- summary(mod)
+tabl = tut$tTable 
+tabl 
+
+# comparison
+anova(mod)
+
+# robust model
+model_robust <- lmer(Base ~ Hand_Finger * Keyboard + (1 | Key), data = data)
+summary(model_robust)
+car::Anova(model_robust)
+
+summary(model_robust)
+parameters::p_value(model_robust)$p<.05
+
+emmeans_model <- emmeans(model_robust, pairwise ~ Hand_Finger )
+summary(emmeans_model) # here we have pairwise comparisons 
+contrasts <- as.data.frame(emmeans_model$contrasts)
+sign_effects <- subset(contrasts, p.value <.05)
+sign_effects
