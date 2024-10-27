04980ded2a2b919de1cd02a6d516612e9876c712
YalmazAbdullah <yaserver@NBP-WorkServer.>
2024-10-27 17:52:28 -0600

    updating structure

--- execute.sh
+++ execute.sh
@@ -1,6 +1,6 @@
-python ./src_py/prepare.py > ./logs/prepare.txt
-python ./src_py/clean.py > ./logs/clean.txt
-python ./src_py/transform_sentence.py > ./logs/transform_sentence.txt
-python ./src_py/transform_keystroke.py
-python ./src_py/transform_triad.py
-python ./src_py/score.py
+python .DiscountEvaluation/src_py/prepare.py > ./logs/prepare.txt
+python .DiscountEvaluation/src_py/clean.py > ./logs/clean.txt
+python .DiscountEvaluation/src_py/transform_sentence.py > ./logs/transform_sentence.txt
+python .DiscountEvaluation/src_py/transform_keystroke.py
+python .DiscountEvaluation/src_py/transform_triad.py
+python .DiscountEvaluation/src_py/score.py
