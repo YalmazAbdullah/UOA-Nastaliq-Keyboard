306272225b3845d20b4344853e7f2e785e0bfa36
Yalmaz <yaserver@NBP-WorkServer.>
2025-03-20 22:45:30 -0600

    api fix fr fr

--- UserStudy/frontend/src/api.js
+++ UserStudy/frontend/src/api.js
@@ -1,4 +1,4 @@
 const apiUrl = import.meta.env.VITE_API_URL;
-const endpoint_dep = apiUrl
+const endpoint_dep = apiUrl+"/"
 const endpoint_dev = "http://127.0.0.1:8000/"
 export const endpoint_live = endpoint_dep
