ca7e904c9b27d6a1dba8ea286d754db46573f2c1
Yalmaz <185710195+YalmazAbdullah@users.noreply.github.com>
2025-08-04 09:50:38 -0600

    data collection commencing
    sanity checks are green

--- UserEvaluation/sanity_check.py
+++ UserEvaluation/sanity_check.py
@@ -14,24 +14,49 @@ def read_json(adress):
 CRULP_MAPPTING = read_json("mappings/CRULP")
 WINDOWS_MAPPING = read_json("mappings/Windows")
 
-target = data["stimulus"][13]
-string_value = data["log"][13]
-log_data = json.loads(string_value)
+# testing milisecond per charachter
+stim = data["stimulus"]
+wpm = data["wpm"]
+time = data["end_time"]-data["start_time"]
+for i in range(62,len(data)):
+    words = stim[i]
+    calc = (data["end_time"][i]-data["start_time"][i])/float(len(words))
+    if abs(wpm[i] - calc)>=0.00000000001:
+        print("error at line %",i)
+        print(wpm[i])
+        print(calc)
+    break
 
-# # check if entered keystrokes match expected
-# target_strokes = ""
-# for char in target:
-#     target_strokes+= CRULP_MAPPTING[char]
+# test for start time misalignment
+for i in range(62,len(data)):
+    if data["start_time"][i] != json.loads(data["log"][i])[0]["timestamp"]:
+        print("start time misaligned")
 
-# logged_strokes = ""
-# for entery in log_data:
-#     key = entery["key"]
-#     print(key)
-#     if (key == "Backspace"):
-#         logged_strokes = logged_strokes[:-1]
-#     elif (len(key)==1):
-#         logged_strokes += key
-# print(logged_strokes == target_strokes)
+
+# check if entered keystrokes match expected final string
+for i in range(62,len(data)):
+    target = data["stimulus"][i]
+    string_value = data["log"][i]
+    log_data = json.loads(string_value)
+    target_strokes = ""
+    for char in target:
+        if data["condition"][i] == "crulp":
+            target_strokes+= CRULP_MAPPTING[char]
+        if data["condition"][i] == "windows":
+            target_strokes+= WINDOWS_MAPPING[char]
+        if data["condition"][i] == "baseline":
+            target_strokes+= char
+
+    logged_strokes = ""
+    for entery in log_data:
+        key = entery["key"]
+        if (key == "Backspace"):
+            logged_strokes = logged_strokes[:-1]
+        elif (len(key)==1):
+            logged_strokes += key
+    if(logged_strokes != target_strokes):
+        print("error at line %",i)
+        break
 
 # #ime target creation
 # string_value = data["log"][22]
@@ -45,17 +70,3 @@ log_data = json.loads(string_value)
 #     elif (len(key)==1):
 #         target += key
 # # print(target)
-
-# testing milisecond per charachter
-stim = data["stimulus"]
-wpm = data["wpm"]
-time = data["end_time"]-data["start_time"]
-for i in range(62,len(data)):
-    words = stim[i]
-    calc = (data["end_time"][i]-data["start_time"][i])/float(len(words))
-    # if abs(wpm[i] - calc)>=0.00000000001:
-    print("error at line %",i)
-    print(wpm[i])
-    print(calc)
-    print(time[i])
-    break
